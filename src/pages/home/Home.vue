c475128c8f47562db2e4ff903900ad4f732e6ef6
wuzhuorui <qiannianfozhan@sina.com>
2018-06-15 23:49:14 +0800

    keep-alive性能优化

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@
   import HomeRecommend from './components/Recommend'
   import HomeWeekend from './components/Weekend'
   import axios from 'axios'
+  import { mapState } from 'vuex'
   export default {
     name: 'Home',
     components: {
@@ -29,12 +30,13 @@
         swiperList: [],
         iconList: [],
         recommendList: [],
-        weekendList: []
+        weekendList: [],
+        lastCity: ''
       }
     },
     methods: {
       getHomeInfo () {
-        axios.get('/api/index.json')
+        axios.get('/api/index.json?city' + this.city)
           .then(this.getHomeInfoSucc)
       },
       getHomeInfoSucc (res) {
@@ -49,10 +51,20 @@
         }
       }
     },
+    computed: {
+      ...mapState(['city'])
+    },
     mounted () {
-      // this.lastCity = this.city
+      this.lastCity = this.city
       this.getHomeInfo()
     },
+    // 配合keep-alive和lastCity临时缓存来优化homt请求
+    activated () {
+      if (this.lastCity !== this.city) {
+        this.lastCity = this.city
+        this.getHomeInfo()
+      }
+    }
   }
 </script>
 
